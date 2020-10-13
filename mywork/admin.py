a3a76235510ad0105dd350e3956493ebcf28f4e8
AndreyS <endry.sh.83.v@gmail.com>
2020-10-13 04:25:50 +0300

    6  фильтр ручной()

--- mywork/admin.py
+++ mywork/admin.py
@@ -3,37 +3,34 @@ from .models import Product, deal, sales_consultant, cashier, accountant
 from math import ceil
 
 
-#admin.site.register(Product)
-
 admin.site.register(deal)
 
 admin.site.register(sales_consultant)
 
 admin.site.register(cashier)
 
-admin.site.register(accountant)
+@admin.register(accountant)
+class AccountantAdmin(admin.ModelAdmin):
+    list_display = ('kanban3', 'kanban4',)
+    search_fields = ('kanban3', 'kanban4',)
+    list_display_links = ('kanban3', 'kanban4',)
 
-@admin.register(Product) #deal
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name_product', 'price',)
     actions = ['discount_20']
 
     def discount_20(self, request, queryset):
         from math import ceil
-        discount = 20  # percentage
+        discount = 20
 
         for product in queryset:
-            """ Set a discount of 20% to selected products """
-            multiplier = discount / 100.  # discount / 100 in python 3
+            """ фильтр уменьшаюший стоимость продукта на 20% """
+            multiplier = discount / 100.
             old_price = product.price
             new_price = ceil(old_price - (old_price * multiplier))
             product.price = new_price
             product.save(update_fields=['price'])
 
-    discount_20.short_description = 'Set 20%% discount'
-
-    # list_display = ('status', 'stat', 'statu', 'gods', 'kanban1', 'kanban2', 'kanban3', 'time', 'price',
-    #                 'added_product', 'update_product')
-    # #list_display_links = ('status', 'gods', 'time', 'price')
-    # search_fields = ('status', 'gods', 'time', 'price')
-#name_product(поле продукта)
+    discount_20.short_description = 'скидка 20%%'
